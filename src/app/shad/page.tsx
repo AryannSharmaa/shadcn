9aad48e4185ef77e2a6489ebcc504c113e273986
Aryan <aryancr72000@gmail.com>
2024-01-15 23:45:01 +0530

    testing shadcn

---
+++ src/app/shad/page.tsx
@@ -0,0 +1,11 @@
+function ShadPage(){
+    return (
+        <main className="h-full flex justify-center items-center flex-col">
+
+        <div>ShadPage 🧭</div>
+        <button className="px-6 py-2 bg-blue-500 rounded my-3 hover:bg-blue-700">Test</button>
+        </main>
+    )
+}
+
+export default ShadPage
